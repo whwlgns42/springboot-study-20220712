1fb844ae1af98a0f8d7d7f51c5a1c191e3bc1483
whwlgns42 <whwlgns42@kakao.com>
2022-07-12 20:57:40 +0900

    init commit

---
+++ src/main/java/com/springboot/studyjihun/StudyJihunApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjihun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJihunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJihunApplication.class, args);
+	}
+
+}
